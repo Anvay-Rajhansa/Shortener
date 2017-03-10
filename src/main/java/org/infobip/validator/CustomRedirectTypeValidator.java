59028b63e36d3dcc654b4f29176da708bcfad472
Anvay R. Rajhansa <aanvayr@gmail.com>
2017-03-10 16:59:40 +0530

    Fixed redirect type validation with null check.

--- src/main/java/org/infobip/validator/CustomRedirectTypeValidator.java
+++ src/main/java/org/infobip/validator/CustomRedirectTypeValidator.java
@@ -16,6 +16,9 @@ public class CustomRedirectTypeValidator implements ConstraintValidator<Validate
 
     @Override
     public boolean isValid(Integer integer, ConstraintValidatorContext constraintValidatorContext) {
+        if(integer == null) {
+            return true;
+        }
         return valueList.contains(integer);
     }
 }
